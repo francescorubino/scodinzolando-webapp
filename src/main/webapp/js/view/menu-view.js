0e22fce78348a2bab0ea572e78b1d0c73ce4ba0e
Marco Chiappini <burundini@gmail.com>
2015-02-04 17:51:22 +0100

    Riaggiunta grafica css Sistemato il menù

--- src/main/webapp/js/view/menu-view.js
+++ src/main/webapp/js/view/menu-view.js
@@ -1,26 +1,22 @@
-define([ 'backbone', 'resthub', 'i18n!nls/labels', 'collection/activity-collection', 'hbs!template/menu-view' ],
-function (Backbone, Resthub, myLabels, ActivityCollection, menuTemplate) {
+define([ 'backbone', 'resthub',  'collection/activity-collection', 'hbs!template/menu-view' ],
+function (Backbone, Resthub,  ActivityCollection, menuTemplate) {
     
     var MenuView = Resthub.View.extend({
         
         // Define view template
         template: menuTemplate,
-        labels : myLabels,
+//        labels : myLabels,
         
         initialize:function () {
         	 // Initialize the collection
-            this.activities = new ActivityCollection();
+            this.collection = new ActivityCollection();
             
-            // Render the view when the activities is retreived from the server
-            this.listenTo(this.activities, 'sync', this.render);
+            // Render the view when the collection is retreived from the server
+            this.listenTo(this.collection, 'sync', this.render);
             
             // Request unpaginated URL
-            this.activities.fetch({ data: { page: 'no'} });
+            this.collection.fetch({ data: { page: 'no'} });
         },
-        
-        render : function(){
-        	MenuView.__super__.render.apply(this, arguments);
-        }
 
     });
     return MenuView;
