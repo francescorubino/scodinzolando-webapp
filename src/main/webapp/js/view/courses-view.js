1eda859a6a7247f041b9fbf19a694159df406447
Marco Chiappini <burundini@gmail.com>
2015-02-11 17:45:59 +0100

    Fix mese iniziale del calendario

--- src/main/webapp/js/view/courses-view.js
+++ src/main/webapp/js/view/courses-view.js
@@ -14,15 +14,27 @@ define([ 'backbone', 'resthub', 'i18n!nls/labels', 'hbs!template/courses-view',
 		render : function() {
 			CoursesView.__super__.render.apply(this, arguments);
 
+			var startDate = this.getStartDate();
+			
 			$('#calendar').fullCalendar({
-				defaultDate : '2015-02-12',
-				editable : true,
-				eventLimit : true,
+				defaultDate : startDate,
+				editable : false,
+				eventLimit : false,
 				events : {
 					url : "calendar/events",
 					cache : true
 				}
 			});
+		},
+		
+		getStartDate : function() {
+			var today = new Date();
+			var yyyy = today.getFullYear();
+			var mm = today.getMonth()+1;
+			if(mm<10) {
+			    mm='0'+mm
+			} 
+			return yyyy+"-"+mm+"-01";
 		}
 
 	});
