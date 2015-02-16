1e631d85df88a8fcbbf9075e15ac42152698dcf8
Marco Chiappini <burundini@gmail.com>
2015-02-16 17:22:55 +0100

    Fixato slider e colorbox

--- src/main/webapp/js/view/slider-view.js
+++ src/main/webapp/js/view/slider-view.js
@@ -30,192 +30,26 @@ define([ 'backbone', 'resthub', 'jssor-slider', 'hbs!template/slider-view',
 			if (this.collection.length !== 0) {
 
 				var options = {
-					$AutoPlay : true, // [Optional]
-					// Whether
-					// to
-					// auto
-					// play,
-					// to
-					// enable
-					// slideshow,
-					// this
-					// option
-					// must
-					// be
-					// set
-					// to
-					// true,
-					// default
-					// value
-					// is
-					// false
-					$SlideDuration : 500, // [Optional]
-					// Specifies
-					// default
-					// duration
-					// (swipe)
-					// for
-					// slide
-					// in
-					// milliseconds,
-					// default
-					// value
-					// is
-					// 500
-					$DragOrientation : 3,
-					$BulletNavigatorOptions : { // [Optional]
-						// Options
-						// to
-						// specify
-						// and
-						// enable
-						// navigator
-						// or
-						// not
-						$Class : $JssorBulletNavigator$, // [Required]
-						// Class
-						// to
-						// create
-						// navigator
-						// instance
-						$ChanceToShow : 2, // [Required]
-						// 0
-						// Never,
-						// 1
-						// Mouse
-						// Over,
-						// 2
-						// Always
-						$AutoCenter : 1, // [Optional]
-						// Auto
-						// center
-						// navigator
-						// in
-						// parent
-						// container,
-						// 0
-						// None,
-						// 1
-						// Horizontal,
-						// 2
-						// Vertical,
-						// 3
-						// Both,
-						// default
-						// value
-						// is 0
-						$Steps : 1, // [Optional]
-						// Steps
-						// to
-						// go
-						// for
-						// each
-						// navigation
-						// request,
-						// default
-						// value
-						// is 1
-						$Lanes : 1, // [Optional]
-						// Specify
-						// lanes
-						// to
-						// arrange
-						// items,
-						// default
-						// value
-						// is 1
-						$SpacingX : 10, // [Optional]
-						// Horizontal
-						// space
-						// between
-						// each
-						// item
-						// in
-						// pixel,
-						// default
-						// value
-						// is 0
-						$SpacingY : 10, // [Optional]
-						// Vertical
-						// space
-						// between
-						// each
-						// item
-						// in
-						// pixel,
-						// default
-						// value
-						// is 0
-						$Orientation : 1
-					// [Optional]
-					// The
-					// orientation
-					// of
-					// the
-					// navigator,
-					// 1
-					// horizontal,
-					// 2
-					// vertical,
-					// default
-					// value
-					// is 1
+					$FillMode : 5,
+					$AutoPlay : true,
+					$SlideDuration : 500,
+					$DragOrientation : 1,
+					$BulletNavigatorOptions : {
+						$Class : $JssorBulletNavigator$,
+						$ChanceToShow : 1,
+						$AutoCenter : 1,
+						$Steps : 1,
+						$Lanes : 1,
+						$SpacingX : 2,
+						$SpacingY : 2,
+						$Orientation : 1,
+						$Lanes : 2
 					},
-					$ArrowNavigatorOptions : { // [Optional]
-						// Options
-						// to
-						// specify
-						// and
-						// enable
-						// arrow
-						// navigator
-						// or
-						// not
-						$Class : $JssorArrowNavigator$, // [Requried]
-						// Class
-						// to
-						// create
-						// arrow
-						// navigator
-						// instance
-						$ChanceToShow : 1, // [Required]
-						// 0
-						// Never,
-						// 1
-						// Mouse
-						// Over,
-						// 2
-						// Always
-						$AutoCenter : 2, // [Optional]
-						// Auto
-						// center
-						// arrows
-						// in
-						// parent
-						// container,
-						// 0
-						// No,
-						// 1
-						// Horizontal,
-						// 2
-						// Vertical,
-						// 3
-						// Both,
-						// default
-						// value
-						// is 0
+					$ArrowNavigatorOptions : {
+						$Class : $JssorArrowNavigator$,
+						$ChanceToShow : 1,
+						$AutoCenter : 2,
 						$Steps : 1
-					// [Optional]
-					// Steps
-					// to
-					// go
-					// for
-					// each
-					// navigation
-					// request,
-					// default
-					// value
-					// is 1
 					}
 				};
 				var jssor_slider1 = new $JssorSlider$("slider1_container",
